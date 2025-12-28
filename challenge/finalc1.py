11ac37962011262cf03483333f459e9585d95b8c
ModerateGmer <thee.chachanan@gmail.com>
2025-12-28 17:34:45 +0700

    hello

--- challenge/finalc1.py
+++ challenge/finalc1.py
@@ -2,7 +2,7 @@ import random
 print("Hello i am the turtle guessing game")
 tutle = int(input("How many turtles? : "))
 wi = int(input("Which turtle will win : "))
-if wi == random.randint(2, tutle):
+if wi == random.randint(1, tutle):
   print("You guessed it right, good boy")
 else:
   print("Try again")
